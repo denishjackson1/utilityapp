e311e835eb8e188adabae56d7fc29c48d7d654cc
denishjackson1 <denishkomakech@gmail.com>
2022-12-04 14:08:50 +0300

    docker file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import Todoform from './components/Todoform';
 import Todolist from './components/Todolist';
+//import {FaBeer} from 'react-icons/fa';
 
 function App() {
   return (
