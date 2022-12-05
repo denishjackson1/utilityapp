a13fea22c562c36e6862c4fc4d02f80a9e975801
denishjackson1 <denishkomakech@gmail.com>
2022-12-05 19:00:05 +0300

    ..

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -42,7 +42,7 @@ function TodoList() {
 
   return (
     <>
-      <h1>Daily Schedules-Denish</h1>
+      <h1>Daily Schedules-Denish.</h1>
       <Todoform onSubmit={addTodo} />
       <Todo
         todos={todos}
