68a9b53e1290e7c8e0a6d7d9d847d86034104712
denishjackson1 <denishkomakech@gmail.com>
2022-12-05 16:41:03 +0300

    Schedules

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -42,7 +42,7 @@ function TodoList() {
 
   return (
     <>
-      <h1>Today's schedule</h1>
+      <h1>Today's Schedules</h1>
       <Todoform onSubmit={addTodo} />
       <Todo
         todos={todos}
